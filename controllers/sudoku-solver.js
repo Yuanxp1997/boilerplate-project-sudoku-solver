4eec293a32f34c1eb050fc9a4f1c8084c9eeab13
Yuanxp1997 <127631889+Yuanxp1997@users.noreply.github.com>
2024-03-31 13:55:50 -0600

    3 tests

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,26 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    if (puzzleString.length !== 81) {
+      return "Expected puzzle to be 81 characters long";
+    }
+    if (puzzleString.match(/[^0-9.]/)) {
+      return "Invalid characters in puzzle";
+    }
+    return "valid";
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    const rowStart = row * 9;
+    const rowEnd = rowStart + 9;
+    const rowValues = puzzleString.slice(rowStart, rowEnd);
+    return !rowValues.includes(value);
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,45 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+const puzzlesAndSolutions =
+  require("../controllers/puzzle-strings.js").puzzlesAndSolutions;
+let solver = new Solver();
 
-suite('Unit Tests', () => {
+suite("Unit Tests", () => {
+  //Logic handles a valid puzzle string of 81 characters
+  test("Logic handles a valid puzzle string of 81 characters", function () {
+    for (let i = 0; i < puzzlesAndSolutions.length; i++) {
+      assert.equal(solver.validate(puzzlesAndSolutions[i][0]), "valid");
+    }
+  });
 
+  //Logic handles a puzzle string with invalid characters (not 1-9 or .)
+  test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", function () {
+    for (let i = 0; i < puzzlesAndSolutions.length; i++) {
+      let puzzle = puzzlesAndSolutions[i][0].replace("1", "A");
+      assert.equal(solver.validate(puzzle), "Invalid characters in puzzle");
+    }
+  });
+
+  //Logic handles a puzzle string that is not 81 characters in length
+  test("Logic handles a puzzle string that is not 81 characters in length", function () {
+    for (let i = 0; i < puzzlesAndSolutions.length; i++) {
+      let puzzle = puzzlesAndSolutions[i][0].slice(0, 80);
+      assert.equal(
+        solver.validate(puzzle),
+        "Expected puzzle to be 81 characters long"
+      );
+    }
+  });
+
+  //Logic handles a valid row placement
+  test("Logic handles a valid row placement", function () {
+    for (let i = 0; i < puzzlesAndSolutions.length; i++) {
+      let puzzle = puzzlesAndSolutions[i][0];
+      for (let j = 0; j < 9; j++) {
+        assert.equal(solver.checkRowPlacement(puzzle, 0, j, puzzle[j]), true);
+      }
+    }
+  });
 });
